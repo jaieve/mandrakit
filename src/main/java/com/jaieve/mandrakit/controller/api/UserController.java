93dda99b16d33bbbdf03aa7a4fa6b4cf8a11eae8
jaieve <yoori0303@naver.com>
2022-11-14 17:25:59 +0900

    [feat] ADD Swagger 설정 추가

--- src/main/java/com/jaieve/mandrakit/controller/api/UserController.java
+++ src/main/java/com/jaieve/mandrakit/controller/api/UserController.java
@@ -2,6 +2,9 @@ package com.jaieve.mandrakit.controller.api;
 
 import com.jaieve.mandrakit.dto.User;
 import com.jaieve.mandrakit.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -18,15 +21,26 @@ import java.util.concurrent.ExecutionException;
 @RequestMapping("/users")
 @Slf4j
 public class UserController {
-//    private
     private final UserService userService;
 
+    @Operation(summary = "사용자 정보 조회", description = "Firebase 컬렉션 Users의 특정 User(문서)")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")})
     @GetMapping
     public ResponseEntity<Object> getUsers(@RequestParam String userKey) throws ExecutionException, InterruptedException {
         User user = userService.getUser(userKey);
         return ResponseEntity.ok().body(user);
     }
 
+    @Operation(summary = "사용자 전체 목록 조회", description = "Firebase 컬렉션 Users의 모든 문서 조회")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")})
     @GetMapping("/list")
     public ResponseEntity<Object> getUsers() throws ExecutionException, InterruptedException {
         List<User> list = userService.getUsers();
